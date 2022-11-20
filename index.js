717bfdeb02c17e3e2bf4422719ea1f4a648010e0
AlifSakib <alifsakib@gmail.com>
2022-11-20 18:45:00 +0600

    Initial Project Setup

---
+++ index.js
@@ -0,0 +1,33 @@
+require("colors");
+require("dotenv").config();
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const express = require("express");
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://doctorsportal:LCEZT2fCqDSJQpEO@cluster0.4mqdriq.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function dbConnect() {
+  try {
+    await client.connect();
+    console.log("Database Connected ".bgGreen);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+dbConnect();
+
+app.listen(port, () => {
+  console.log(`Server is listening to port ${port} `.bgGreen);
+});
